f9b0ed47dcbd34e60ff4630d83e919646e7f50a1
XL <x@xanderli.com>
2016-02-22 22:12:11 -0500

    TCP tests

--- constant.py
+++ constant.py
@@ -6,8 +6,13 @@ device_id = 1
 
 # TCP information
 TCP_IP = '172.25.47.127'
-
+TCP_PORT = '5005'
 # API information
-URL = 'http://greenwall.elasticbeanstalk.com/api/log/'
-USERNAME = 'root'
+API_URL = 'http://greenwall.elasticbeanstalk.com/api/log/'
+API_USERNAME = 'root'
 API_KEY = '123'
+
+
+# Mode information
+Mode_record = 1
+Mode_control = 2
--- runner.py
+++ runner.py
@@ -1,13 +1,17 @@
 import glob
 import time
 import serial
+import json
+import requests
+from constant import *
 Arduino_connection = False
 time.sleep(2)
-
+mode = Mode_record
 
 while True:
     time.sleep(1)
     device_list=[]
+    ser = None
     try:
         device_list = glob.glob('/dev/tty.usb*')
         print device_list[0]
@@ -16,13 +20,23 @@ while True:
     except IndexError:
         print 'Failed to connect to Arduino, Check USB connection'
 
-
     while True:
         if Arduino_connection:
-            try:
-                data = ser.readline()
-            except AssertionError:
-                print 'Serial reading failed'
+            if mode == Mode_record:
+                try:
+                    data = ser.readline()
+                    j_data = json.loads(data)
+                    params = {'username':+API_USERNAME, 'api_key':API_KEY, 'format':'json'}
+                    headers = {'content-type': 'application/json'}
+                    requests.post(API_URL, params=params, data=data, headers=headers)
+                except ValueError:
+                    print 'invalid data reading'
+                except AssertionError:
+                    print 'Serial reading failed'
+            if mode == Mode_control:
+                pass
         else:
             break
+
+
 
